239aa0eee0e83faab1f16ed4e43d893871a510f5
Solomon <solomondanso2023@gmail.com>
2023-07-17 18:25:57 +0000

    Chapter 4

--- Tutorials/app/src/main/java/com/hydotgroup/tutorials/MainActivity.java
+++ Tutorials/app/src/main/java/com/hydotgroup/tutorials/MainActivity.java
@@ -4,20 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btn1);
+
 
     }
+    public void clickMe(View v){
+        Toast.makeText(this, "I have clicked on the button", Toast.LENGTH_LONG).show();
+        EditText editText = findViewById(R.id.edit1);
 
-    public void func1(View view){
-TextView txt = findViewById(R.id.money1);
-txt.setText("Sika 2");
-    }
+
+        TextView txt = findViewById(R.id.txt1);
+        txt.setText("Name: "+editText.getText().toString());
+
+
+           }
+
 }
