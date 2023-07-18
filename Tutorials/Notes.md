0f39c02daefd0ba6687ab78c6363b88248973c80
Solomon <solomondanso2023@gmail.com>
2023-07-18 15:36:36 +0000

    Chapter 5

--- Tutorials/Notes.md
+++ Tutorials/Notes.md
@@ -1,3 +1,106 @@
-Chapter 4 
-We can use Toast to display information like a dialog box
-Toast.makeText(this, "Waw", Toast.LENGTH_SHORT).show();
+Chapter 5 
+- The switch in the View interface will give an error, change it to an if else statement 
+- protected void onCreate(Bundle savedInstanceState) this will be where all the ui element will be initialised
+- In the right way, we will initialised the variables first then use it like the example below
+
+
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+xmlns:app="http://schemas.android.com/apk/res-auto"
+xmlns:tools="http://schemas.android.com/tools"
+android:layout_width="match_parent"
+android:layout_height="match_parent"
+tools:context=".MainActivity"
+android:padding="10dp"
+>
+<EditText
+android:layout_width="wrap_content"
+android:layout_height="wrap_content"
+android:hint="Enter Your Details"
+android:layout_centerHorizontal="true"
+android:layout_centerVertical="true"
+android:id="@+id/edit1"
+/>
+
+<Button
+android:layout_width="wrap_content"
+android:layout_height="wrap_content"
+android:text="Submit"
+android:id="@+id/Clicker"
+android:layout_below="@+id/edit1"
+android:layout_marginTop="10dp"
+android:layout_centerHorizontal="true"
+android:background="@color/black"
+android:textColor="@color/white"
+android:textSize="20sp"
+
+
+    />
+
+    <TextView
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:layout_below="@+id/Clicker"
+        android:textColor="@color/red"
+        android:text="The Text"
+        android:textAlignment="center"
+        android:layout_marginTop="10dp"
+        android:id="@+id/txt1"
+
+
+        />
+
+</RelativeLayout>
+
+package com.hydotgroup.tutorials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+//Declaration Location
+public TextView text1;
+public EditText edit1;
+public Button btn1;
+
+// Usage Location
+@Override
+public void onClick(View v) {
+int id = v.getId();
+if(id==R.id.Clicker){
+text1.setText(edit1.getText());
+Toast.makeText(this, "You have Clicked Me", Toast.LENGTH_SHORT).show();
+}
+}
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Instantiate Location
+
+        text1 = findViewById(R.id.txt1);
+        text1.setOnClickListener(this);
+
+        edit1 = findViewById(R.id.edit1);
+        edit1.setOnClickListener(this);
+
+        btn1 = findViewById(R.id.Clicker);
+        btn1.setOnClickListener(this);
+
+
+
+    }
+
+
+
+
+}
--- Tutorials/app/src/main/java/com/hydotgroup/tutorials/MainActivity.java
+++ Tutorials/app/src/main/java/com/hydotgroup/tutorials/MainActivity.java
@@ -3,32 +3,113 @@ package com.hydotgroup.tutorials;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //Declaration Location
+private CheckBox Harry,Sweety,Baby;
+private RadioGroup maritalStatus;
+private RadioGroup rdgrp;
+private ProgressBar progressBar,progressBar2;
 
+    @Override
+    public void onClick(View v) {
+        int id  = v.getId();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.btn1);
+
+        Harry = findViewById(R.id.Harry);
+        Baby = findViewById(R.id.Baby);
+        Sweety = findViewById(R.id.Sweety);
+        rdgrp = findViewById(R.id.rdGroup);
+        progressBar = findViewById(R.id.progBar);
+        progressBar2 = findViewById(R.id.progBar2);
+
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run(){
+               for(int i=0; i<10; i++ ){
+                   progressBar2.incrementProgressBy(10);
+                   SystemClock.sleep(500);
+
+               }
+            }
+        });
+        thread.start();
+
+
+
+
+
+        //Check to see the default radio button checked
+
+        int checkedId = rdgrp.getCheckedRadioButtonId();
+        if(checkedId==R.id.rbMarried){
+            Toast.makeText(MainActivity.this, "Aware ye de", Toast.LENGTH_SHORT).show();
+        } else if (checkedId==R.id.rbSingle) {
+            Toast.makeText(MainActivity.this, "Loneliness will finish you kwata kwata", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Mo na mo bre", Toast.LENGTH_SHORT).show();
+        }
+
+
+        //Based on what the user clicks, perform this action
+        rdgrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId==R.id.rbMarried){
+                    Toast.makeText(MainActivity.this, "Aware ye de", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                } else if (checkedId==R.id.rbSingle) {
+                    Toast.makeText(MainActivity.this, "Loneliness will finish you kwata kwata", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Mo na mo bre", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        if(Harry.isChecked()){
+            Toast.makeText(this, "Greetings Harry", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Oh Harry", Toast.LENGTH_SHORT).show();
+        }
+        Harry.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+              if(isChecked){
+                  Toast.makeText(MainActivity.this, "Hello Harry", Toast.LENGTH_SHORT).show();
+              }
+              else{
+                  Toast.makeText(MainActivity.this, "Bye Harry", Toast.LENGTH_SHORT).show();
+              }
+            }
+        });
+
+
+
 
 
     }
-    public void clickMe(View v){
-        Toast.makeText(this, "I have clicked on the button", Toast.LENGTH_LONG).show();
-        EditText editText = findViewById(R.id.edit1);
 
 
-        TextView txt = findViewById(R.id.txt1);
-        txt.setText("Name: "+editText.getText().toString());
 
-
-           }
 
 }
